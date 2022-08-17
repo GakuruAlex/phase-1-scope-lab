0a91b0d812801f9419735e9aa30f74cfb0b5ceef
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-17 16:53:09 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var  customerName =`bob`;
+
+const leastFavoriteCustomer =`Doe`;
+
+
+function setBestCustomer (){
+
+    return bestCustomer = `not bob`;
+}
+
+function overwriteBestCustomer (){
+
+return bestCustomer= 'maybe bob';
+
+}
+
+function changeLeastFavoriteCustomer(){
+
+    leastFavoriteCustomer ='Doe';
+
+}
+
+function upperCaseCustomerName(){
+
+return  customerName = customerName.toUpperCase();
+
+}
+
